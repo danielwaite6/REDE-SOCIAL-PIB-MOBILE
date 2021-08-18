41d6df62da3fc743f6907f8b1e58fb26f97a66e3
Daniel Waite <danielwaite6@hotmail.com>
2021-08-18 03:42:42 -0300

    first commit

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: 'rgba(48, 156, 156, 0.3)';
+  background-color: 'rgba(1, 10, 10, 0.842)';
   justify-content: center;
   align-items: center;
 `;
